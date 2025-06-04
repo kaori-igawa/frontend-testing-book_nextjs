f4cb0209db7321e296e47d20e92c5da66abcaead
kaori-igawa <kaori0v0igawa@gmail.com>
2025-06-04 15:09:20 +0900

    [8-2] Storybookの必須アドオン

---
+++ src/components/layouts/BasicLayout/Header/index.stories.tsx
@@ -0,0 +1,32 @@
+import { handleGetMyProfile } from '@/services/client/MyProfile/__mock__/msw';
+import { LoginUserInfoProviderDecorator, SPStory } from '@/tests/storybook';
+import { expect } from '@storybook/jest';
+import { ComponentMeta, ComponentStoryObj } from '@storybook/react';
+import { userEvent as user, waitFor, within } from '@storybook/testing-library';
+import { Header } from './';
+
+export default {
+  component: Header,
+  decorators: [LoginUserInfoProviderDecorator],
+} as ComponentMeta<typeof Header>;
+
+type Story = ComponentStoryObj<typeof Header>;
+
+export const NotLoggedIn: Story = {
+  parameters: {
+    msw: { handlers: [handleGetMyProfile({ status: 401})]},
+  },
+}
+
+export const LoggedIn: Story = {};
+
+export const SPLoggedIn: Story = {
+  parameters: {
+    ...SPStory.parameters,
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const navigation = canvas.queryByRole('navigation', { name: 'ナビゲーション'});
+    await expect(navigation).not.toBeInTheDocument();
+  }
+};
