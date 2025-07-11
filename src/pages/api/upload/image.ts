04a2cc198abf57fd43ceedd09db95805ccc68754
kaori-igawa <kaori0v0igawa@gmail.com>
2025-07-11 18:20:16 +0900

    minioにアップロードできない不具合解消

--- src/pages/api/upload/image.ts
+++ src/pages/api/upload/image.ts
@@ -5,6 +5,8 @@ const s3 = new S3({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   endpoint: process.env.AWS_S3_ENDPOINT,
+  s3ForcePathStyle: true,
+  signatureVersion: 'v4',
 });
 
 const handleGet = withLogin(async (req, res) => {
