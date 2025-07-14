eaa5e1592b7d74a2a0c34275bdbc3a34afe80f12
kaori-igawa <kaori0v0igawa@gmail.com>
2025-07-14 12:09:06 +0900

    [10-10] 記事編集ページのE2Eテスト

--- e2e/MyPostEdit.spec.ts
+++ e2e/MyPostEdit.spec.ts
@@ -26,4 +26,41 @@ test.describe('記事編集ページ', () => {
     await page.waitForNavigation();
     await expect(page).toHaveTitle(newTitle);
   });
+
+  test('下書き記事を公開できる', async ({ page }) => {
+    const title = '下書き公開テスト';
+    await gotoAndCreatePostAsDraft({ page, title, userName });
+    await gotoEditPostPage({ page, title });
+    await page.getByText('公開ステータス').click();
+    await page.getByRole('button', { name: '記事を公開する' }).click();
+    await page.getByRole('button', { name: 'はい' }).click();
+    await page.waitForNavigation();
+    await expect(page).toHaveTitle(title);
+  });
+
+  test('公開記事を非公開にできる', async ({ page }) => {
+    const title = '記事非公開テスト';
+    await gotoAndCreatePostAsPublish({ page, title, userName });
+    await gotoEditPostPage({ page, title });
+    await page.getByText('公開ステータス').click();
+    await page.getByRole('button', { name: '下書き保存する' }).click();
+    await page.waitForNavigation();
+    await expect(page).toHaveTitle(title);
+  });
+
+  test('公開記事を削除できる', async ({ page }) => {
+    const title = '記事削除テスト';
+    await gotoAndCreatePostAsPublish({ page, title, userName });
+    await gotoEditPostPage({ page, title });
+    await page.getByRole('button', { name: '記事を削除する' }).click();
+    await page.getByRole('button', { name: 'はい' }).click();
+    await page.waitForNavigation();
+    await expect(page).toHaveTitle(`${userName}さんの投稿記事一覧`);
+  });
+
+  test('アクセシビリティ検証', async ({ page }) => {
+    await page.goto(url(path));
+    await injectAxe(page as any);
+    await checkA11y(page as any);
+  });
 });
